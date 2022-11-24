21b11a3d034af48d95efe9c3d849d81c5259b02c
webworkstudent <116783503+webworkstudent@users.noreply.github.com>
2022-11-24 14:19:31 +0530

    Update python_menu.php

--- python_menu.php
+++ python_menu.php
@@ -1,1 +1,160 @@
 
+	   <div class="sidenav"><h2>Python</h2><details>
+		<summary class="sum">👉<b>Python Tutorial</b></summary>
+		
+            <a href="">Python HOME</a>
+            <a href="">Python Intro</a>
+            <a href="">Python Get Started</a>
+            <a href="">Python Syntax</a>
+            <a href="">Python Comments</a>
+            <a href="">Python Variables</a>
+            <a href="">Python Data Types</a>
+            <a href="">Python Numbers</a>
+            <a href="">Python Casting</a>
+            <a href="">Python Strings</a>
+
+            <a href="">Python Booleans</a>
+            <a href="">Python Operators</a>
+            <a href="">Python Lists</a>
+            <a href="">Python Tuples</a>
+            <a href="">Python Sets</a>
+            <a href="">Python Dictionaries</a>
+            <a href="">Python If...Else</a>
+            <a href="">Python While Loops</a>
+            <a href="">Python For Loops</a>
+            <a href="">Python Functions</a>
+            <a href="">Python Lambda</a>
+            <a href="">Python Arrays</a>
+            <a href="">Python Classes/Objects</a>
+            <a href="">Python Inheritance</a>
+            <a href="">Python Iterators</a>
+            <a href="">Python Scope</a>
+            <a href="">Python Modules</a>
+            <a href="">Python Dates</a>
+            <a href="">Python Math</a>
+            <a href="">Python JSON</a>
+            <a href="">Python RegEx</a>
+            <a href="">Python PIP</a>
+            <a href="">Python Try...Except</a>
+            <a href="">Python User Input</a>
+            <a href="">Python String Formatting</a></details><br>
+
+            <details><summary>👉<b>File Handling</b></summary>
+            <a href="">Python File Handling</a>
+            <a href="">Python Read Files</a>
+            <a href="">Python Write/Create Files</a>
+            <a href="">Python Delete Files</a></details><br>
+
+            <details><summary>👉<b>Python Modules</b></summary>
+            <a href="">NumPy Tutorial</a>
+            <a href="">Pandas Tutorial</a>
+            <a href="">SciPy Tutorial</a>
+            <a href="">Django Tutorial</a></details><br>
+
+            <details><summary>👉<b>Python Matplotlib</b></summary>
+            <a href="">Matplotlib Intro</a>
+            <a href="">Matplotlib Get Started</a>
+            <a href="">Matplotlib Pyplot</a>
+            <a href="">Matplotlib Plotting</a>
+            <a href="">Matplotlib Markers</a>
+            <a href="">Matplotlib Line</a>
+            <a href="">Matplotlib Labels</a>
+            <a href="">Matplotlib Grid</a>
+            <a href="">Matplotlib Subplot</a>
+            <a href="">Matplotlib Scatter</a>
+            <a href="">Matplotlib Bars</a>
+            <a href="">Matplotlib Histograms</a>
+            <a href="">Matplotlib Pie Charts</a></details><br>
+
+            <details><summary>👉<b>Machine Learning</b></summary>
+            <a href="">Getting Started</a>
+            <a href="">Mean Median Mode</a>
+            <a href="">Standard Deviation</a>
+            <a href="">Percentile</a>
+            <a href="">Data Distribution</a>
+            <a href="">Normal Data Distribution</a>
+
+            <a href="">Scatter Plot</a>
+            <a href="">Linear Regression</a>
+            <a href="">Polynomial Regression</a>
+            <a href="">Multiple Regression</a>
+            <a href="">Scale</a>
+            <a href="">Train/Test</a>
+            <a href="">Decision Tree</a>
+            <a href="">Confusion Matrix</a>
+            <a href="">Hierarchical Clustering</a>
+            <a href="">Logistic Regression</a>
+            <a href="">Grid Search</a>
+            <a href="">Categorical Data</a>
+            <a href="">K-means</a>
+            <a href="">Bootstrap Aggregation</a>
+            <a href="">Cross Validation</a>
+            <a href="">AUC - ROC Curve</a>
+            <a href="">K-nearest neighbors</a></details><br>
+
+            <details><summary>👉<b>Python MySQL</b></summary>
+            <a href="">MySQL Get Started</a>
+            <a href="">MySQL Create Database</a>
+            <a href="">MySQL Create Table</a>
+            <a href="">MySQL Insert</a>
+            <a href="">MySQL Select</a>
+            <a href="">MySQL Where</a>
+            <a href="">MySQL Order By</a>
+            <a href="">MySQL Delete</a>
+            <a href="">MySQL Drop Table</a>
+            <a href="">MySQL Update</a>
+            <a href="">MySQL Limit</a>
+            <a href="">MySQL Join</a></details><br>
+
+            <details><summary>👉<b>Python MongoDB</b></summary>
+            <a href="">MongoDB Get Started</a>
+            <a href="">MongoDB Create Database</a>
+            <a href="">MongoDB Create Collection</a>
+            <a href="">MongoDB Insert</a>
+            <a href="">MongoDB Find</a>
+            <a href="">MongoDB Query</a>
+            <a href="">MongoDB Sort</a>
+            <a href="">MongoDB Delete</a>
+            <a href="">MongoDB Drop Collection</a>
+            <a href="">MongoDB Update</a>
+            <a href="">MongoDB Limit</a></details><br>
+
+            <details><summary>👉<b>Python Reference</b></summary>
+            <a href="">Python Overview</a>
+            <a href="">Python Built-in Functions</a>
+            <a href="">Python String Methods</a>
+            <a href="">Python List Methods</a>
+            <a href="">Python Dictionary Methods</a>
+            <a href="">Python Tuple Methods</a>
+            <a href="">Python Set Methods</a>
+            <a href="">Python File Methods</a>
+            <a href="">Python Keywords</a>
+            <a href="">Python Glossary</a></details><br>
+
+            <details><summary>👉<b>Module Reference</b></summary>
+            <a href="">Random Module</a>
+            <a href="">Requests Module</a>
+            <a href="">Statistics Module</a>
+            <a href="">Math Module</a>
+            <a href="">cMath Module</a></details><br>
+
+            <details><summary>👉<b>Python How To</b></summary>
+            <a href="">Remove List Duplicates</a>
+            <a href="">Reverse a String</a>
+            <a href="">Add Two Numbers</a></details><br>
+
+            <details><summary>👉<b>Python Examples</b></summary>
+            <a href="">Python Examples</a>
+            <a href="">Python Compiler</a>
+            <a href="">Python Exercises</a>
+            <a href="">Python Quiz</a>
+            <a href="">Python Certificate</a></details>
+            <br>
+                </details>
+                    </div>
+
+
+                <div class="main">
+                    <h2> HTML</h2>
+                    <p> Afridi took 2/24 while Mohammad Nawaz took 1/12.</p>
+                </div>
